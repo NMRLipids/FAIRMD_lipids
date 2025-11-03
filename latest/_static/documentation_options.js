f29719fd2d0719c82a3c2662f07f5827c36bff82
markussmiettinen <markussmiettinen@users.noreply.github.com>
2025-11-03 13:47:21 +0000

    deploy: 3fe07e15dbdee0e6a2f3c039a05e0effd82e4424

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.1.dev25+g1b0521d02',
+    VERSION: '1.3.1.dev26+g3fe07e15d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
