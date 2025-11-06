5a16054fd0d67e642639b37c7ebede97cb7d700e
comcon1 <comcon1@users.noreply.github.com>
2025-11-06 08:32:24 +0000

    deploy: 89af072584be43d2f9ed83830348158f17c5fffe

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.2a1.dev5+g163bfa302',
+    VERSION: '1.3.2a1.dev6+g89af07258',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
