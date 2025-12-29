c2dfc54a32f2b52a1355b881a90da8531491bca4
MagnusSletten <MagnusSletten@users.noreply.github.com>
2025-12-29 16:16:41 +0000

    deploy: f5504ef8f01c2f7cbf197dc048e28046dac2ef67

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.2a1.dev47+g338c9370f',
+    VERSION: '1.3.2a1.dev48+gf5504ef8f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
